61f24aadbbdf88cd71a6a19685d33fc55fc0ef83
nicolas <nicolas@nicolas.hodiesne>
2022-10-16 18:53:19 +0200

    Fix: problème filtres avec le nouveau format du champ création

--- controllers/top.js
+++ controllers/top.js
@@ -7,7 +7,7 @@ module.exports = {
         // TODO: automate tests for filters
         const filters = {
             title: request.query.title || '',
-            maxCreation: parseInt(request.query.maxCreation) || today.getFullYear(),
+            maxCreation: parseInt(request.query.maxCreation) || today,
             minCreation: parseInt(request.query.minCreation) || 0,
             maxVisible: new Date(request.query.maxVisible ? request.query.maxVisible + 'T00:00:00.000Z' : today.toISOString()),
             minVisible: new Date(request.query.minVisible ? request.query.minVisible + 'T00:00:00.000Z' : 0),
